71de2acc8524b4e83ae693e46688ca4ac53d5374
Abhijeet Pratap Singh <abhijeet.singh@easyrewardz.com>
2026-07-20 00:36:29 +0530

    Add proper TypeScript declarations for next-themes

--- @types/next-themes/index.d.ts
+++ @types/next-themes/index.d.ts
@@ -1,1 +1,13 @@
-declare module 'next-themes';
+import { ReactNode } from 'react';
+
+declare module 'next-themes' {
+  interface ThemeProviderProps {
+    attribute?: 'class' | 'data-theme';
+    defaultTheme?: string;
+    enableSystem?: boolean;
+    disableTransitionOnChange?: boolean;
+    storageKey?: string;
+    value?: string;
+  }
+  export const ThemeProvider: React.FC<ThemeProviderProps & { children: ReactNode }>;
+}
